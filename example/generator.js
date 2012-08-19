163c367998e863741a0a937bee21db18b8722ceb
Florian Margaine <florian@margaine.com>
2012-08-20 01:48:49 +0200

    Better example

--- example/generator.js
+++ example/generator.js
@@ -1,8 +1,16 @@
-var generator = require( 'docgenerator' );
+var generator = require( 'docgenerator' ),
+    fs = require( 'fs' );
+
+// Get all the markdown files in this folder
+var files = fs.readdirSync( '.' );
+
+files = files.filter( function( file ) {
+    return file.substr( -3 ) === '.md';
+});
 
 generator
     .set( 'format', 'book' )
     .set( 'output', 'documentation.html' )
-    .set( 'input', [ 'some.md', 'some2.md' ] )
+    .set( 'input', files )
     .generate();
 
